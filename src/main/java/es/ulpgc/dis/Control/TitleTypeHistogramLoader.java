7e379c8fb632bdc1d360ffe66ee5dec22d6c44d5
Eva <evaayuaan@gmail.com>
2024-12-04 13:27:35 +0000

    Main modified and createMapHistogram function added

--- src/main/java/es/ulpgc/dis/Control/TitleTypeHistogramLoader.java
+++ src/main/java/es/ulpgc/dis/Control/TitleTypeHistogramLoader.java
@@ -2,7 +2,9 @@ package es.ulpgc.dis.Control;
 
 import es.ulpgc.dis.Model.Histogram;
 import es.ulpgc.dis.Model.Title;
+import es.ulpgc.dis.io.TSVTitleReader;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,6 +15,13 @@ public class TitleTypeHistogramLoader implements HistogramLoader{
         for (Title title : titles) {
             histogram.put(title.titleType(), histogram.getOrDefault(title.titleType(), 0) + 1);
         }
+        return histogram;
+    }
+
+    public Map<Title.TitleType, Integer> createMapHistogram(String archive) {
+        File file = new File(archive);
+        List<Title> titles = new TSVTitleReader(file).read();
+        Map<Title.TitleType, Integer> histogram = new TitleTypeHistogramLoader().generateMapHistogram(titles);
         return histogram;
     }
 
--- src/main/java/es/ulpgc/dis/Main.java
+++ src/main/java/es/ulpgc/dis/Main.java
@@ -10,15 +10,7 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        File file = new File(".\\src\\main\\resources\\title.basics.tsv");
-        List<Title> titles = new TSVTitleReader(file).read();
-        Map<Title.TitleType, Integer> histogram = new TitleTypeHistogramLoader().generateMapHistogram(titles);
-        printHistogram(histogram);
+
     }
 
-    private static void printHistogram(Map<Title.TitleType, Integer> histogram) {
-        for (Title.TitleType titleType : histogram.keySet()) {
-            System.out.println(titleType + ": " + histogram.get(titleType));
-        }
-    }
 }
