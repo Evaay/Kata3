7720b8e825cddf7272980f514fb30299541fcc39
Eva <evaayuaan@gmail.com>
2024-12-04 13:52:01 +0000

    kata3 finished

--- src/main/java/es/ulpgc/dis/Main.java
+++ src/main/java/es/ulpgc/dis/Main.java
@@ -2,13 +2,7 @@ package es.ulpgc.dis;
 
 import es.ulpgc.dis.Control.HistogramLoader;
 import es.ulpgc.dis.Control.TitleTypeHistogramLoader;
-import es.ulpgc.dis.Model.Title;
 import es.ulpgc.dis.View.MainFrame;
-import es.ulpgc.dis.io.TSVTitleReader;
-
-import java.io.File;
-import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
