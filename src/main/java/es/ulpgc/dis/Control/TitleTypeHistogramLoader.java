e876efbde3ad93c14646fe9a49fd331af57793be
Eva <evaayuaan@gmail.com>
2024-12-04 13:25:26 +0000

    generate function name changed to generateMapHistogram

--- src/main/java/es/ulpgc/dis/Control/TitleTypeHistogramLoader.java
+++ src/main/java/es/ulpgc/dis/Control/TitleTypeHistogramLoader.java
@@ -1,17 +1,23 @@
 package es.ulpgc.dis.Control;
 
+import es.ulpgc.dis.Model.Histogram;
 import es.ulpgc.dis.Model.Title;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class TitleTypeHistogramLoader {
-    public Map<Title.TitleType, Integer> generate(List<Title> titles) {
+public class TitleTypeHistogramLoader implements HistogramLoader{
+    public Map<Title.TitleType, Integer> generateMapHistogram(List<Title> titles) {
         Map<Title.TitleType, Integer> histogram = new HashMap<>();
         for (Title title : titles) {
             histogram.put(title.titleType(), histogram.getOrDefault(title.titleType(), 0) + 1);
         }
         return histogram;
+    }
+
+    @Override
+    public Histogram load(String archive) {
+        return null;
     }
 }
